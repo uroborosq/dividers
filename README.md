b58a53ef482932a564b8ff8ddc3649c7acbb7e22
Dmitriy Tikhomirov <uroborosq@ya.ru>
2025-11-21 01:54:51 +0300

    comments, readme, taskfile

---
+++ README.md
@@ -0,0 +1,25 @@
+# Dividers
+
+Count dividers
+
+## Requirements
+
+Install Go compiler and taskfile
+
+Windows
+```sh
+winget install -e --id GoLang.Go
+winget install Task.Task
+
+refreshenv
+```
+
+## Building
+```sh
+task build
+```
+
+## Running
+```sh
+task run
+```
--- main.go
+++ main.go
@@ -5,18 +5,38 @@ import (
 	"os"
 )
 
-func main() {
+func getWorkingDirectory() (string, error) {
+	// Проверяем, переопределил ли пользователь рабочую папку.
+	// Первый аргумент всегда - название самой программы, а вот если есть второй - значит переопределил
+	// И мы его берем используем
+	if len(os.Args) == 2 {
+		return os.Args[1], nil
+	}
+
+	// Если пользователь ничего не указывал, то используем текущую рабочую папку (из которой открыта программа)
 	wd, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("failed to get current working directory: %w", err)
+	}
+
+	return wd, nil
+}
+
+func main() {
+	// Получить путь к рабочей папке, из которой будет начат поиск исходного файла.
+	workingDirectory, err := getWorkingDirectory()
 	if err != nil {
 		panic(err)
 	}
 
-	filePath, choosed := chooseFile(wd)
+	// Запускаем диалог поиска нужного исходного файла в заданной рабочей папке.
+	filePath, choosed := chooseFile(workingDirectory)
 	if choosed {
 		fmt.Println("no file choose, exiting")
 		return
 	}
 
+	// Считываем исходные данные из найденного файла.
 	err = parseFile(filePath)
 	if err != nil {
 		panic(err)
